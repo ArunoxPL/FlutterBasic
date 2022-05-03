71969232e75db23e87dcf7c1e30e0ef1e61fb84b
Greg <greg.malek@gmail.com>
2022-05-03 22:34:43 +0200

    Adding basic functions

--- training_app/lib/main.dart
+++ training_app/lib/main.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answered");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("The question!"),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
